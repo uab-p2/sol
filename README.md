9014c4a532dad437ef0565ce7c1b6abf900de118
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2026-06-13 12:39:55 +0200

    feat/basic project structure and prototype quest

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align=center style="text-align: center">
+  <img src="doc/img/sol_logo.png" width="200" />
+</p>
+
+## Estructura
+* [quest/](quest/): retos del proyecto SOL
+* [src/](src/): base de código común para todos los quests
+* [doc/](doc/): documentación del proyecto
+* [test/](test/): tests unitarios de la base de código
+* [tool/](tool/): herramientas para el mantenimiento del proyecto
---
+++ src/cat.cpp
@@ -0,0 +1,33 @@
+#include "cat.h"
+
+void Cat::feed() {
+    m_hungry = false;
+    m_sleepy = true;
+}
+
+bool Cat::pet() {
+    bool success = false;
+    if (!m_hungry) {
+        success = true;
+        m_sleepy = true;
+        m_hungry = true;
+    }
+    return success;
+}
+
+void Cat::sleep() {
+    m_sleepy = false;
+    m_hungry = true;
+}
+
+std::string Cat::meow() {
+    std::string message = "Meow! (\"I am '" + m_name + "'.";
+    if (m_sleepy) {
+        message += " I am sleepy.";
+    }
+    if (m_hungry) {
+        message += " AND I'M HUNGRY!";
+    }
+    message += ")";
+    return message;
+}
