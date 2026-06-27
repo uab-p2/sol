adcc89645f3d82cca1504e0fb639b0dcedfbd7b0
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2026-06-27 19:00:23 +0200

    refactor(site): fold tag.py into quest.py for a cleaner query interface

--- doc/guide/main.py
+++ doc/guide/main.py
@@ -6,7 +6,7 @@ import sys
 from pathlib import Path
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from tool.tag import PROJECT_ROOT, DEFAULT_QUEST_DIR, list_tags, list_metadata_for_tag
+from tool.quest import PROJECT_ROOT, DEFAULT_QUEST_DIR, Quest, Tag
 
 GITHUB_QUEST_URL = "https://github.com/uab-p2/sol/tree/main/quest"
 
@@ -20,7 +20,7 @@ def define_env(env):
     @env.macro
     def tag_title(tag_name):
         try:
-            tag = [t for t in list_tags() if t.name == tag_name][0]
+            tag = [t for t in Tag.list() if t.name == tag_name][0]
             return tag.title
         except IndexError:
             return ""
@@ -28,7 +28,7 @@ def define_env(env):
     @env.macro
     def tag_description(tag_name):
         try:
-            tag = [t for t in list_tags() if t.name == tag_name][0]
+            tag = [t for t in Tag.list() if t.name == tag_name][0]
             return tag.description
         except IndexError:
             return ""
@@ -36,13 +36,11 @@ def define_env(env):
     @env.macro
     def tag_quests(tag_name):
         """List the quests of a given tag (campaign)."""
+        quests = [quest for quest in Quest.list() if any(tag.name == tag_name for tag in quest.tags)]
         lines = []
-        # for quest_metadata in list_metadata_for_tag(DEFAULT_QUEST_DIR, tag_name):
-        #     lines.append("* [" + quest_metadata.title + "](" + GITHUB_QUEST_URL + "/" + os.path.basename(
-        #         quest_metadata.module_path.resolve().as_posix()) + ")")
-        for quest_metadata in list_metadata_for_tag(DEFAULT_QUEST_DIR, tag_name):
-            lines.append("### [" + quest_metadata.title + "](" + GITHUB_QUEST_URL + "/" + os.path.basename(
-                quest_metadata.module_path.resolve().as_posix()) + ")")
-            lines.append(quest_metadata.description.strip().split("\n\n")[0])
+        for quest in quests:
+            lines.append("### [" + quest.title + "](" + GITHUB_QUEST_URL + "/" + os.path.basename(
+                quest.module_path.resolve().as_posix()) + ")")
+            lines.append(quest.description.strip().split("\n\n")[0])
             lines.append("")
         return "\n".join(lines)
