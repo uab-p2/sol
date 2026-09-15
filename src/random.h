0a36d782edad7b9ef48c666612f73b5fa1f38e55
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2026-09-15 16:26:25 +0200

    fix(src): add example usage to Random's doxygen comments

--- src/random.h
+++ src/random.h
@@ -5,9 +5,21 @@ class Random {
 public:
     /// @return a random integer uniformly distributed
     ///   in the range [a, b] (inclusive).
+    ///
+    /// Example:
+    /// @code
+    /// int roll = Random::get_int(1, 6);
+    /// @endcode
     static int get_int(int a, int b);
 
     /// @return true with probability `probability`
     ///   (in the range [0, 1]), false otherwise.
+    ///
+    /// Example:
+    /// @code
+    /// if (Random::get_bool(0.1)) {
+    ///     std::cout << "rare event" << std::endl;
+    /// }
+    /// @endcode
     static bool get_bool(double probability = 0.5);
 };
